bb137468fc66672016924cc961c9f837016c7232
Eren Akarsu <76885282+lordkli@users.noreply.github.com>
2022-09-22 00:29:51 +0200

    third commit

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import "../../App.css";
 import Cards from "../Cards";
+import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 
 function Home() {
   return (
     <>
       <HeroSection />
-      <Cards/>
+      <Cards />
+      <Footer />
     </>
   );
 }
